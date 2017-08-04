0e9277a9b7506356d0d46cd575b1614b6608a1b2
maps4all <maps4all007@yandex.com>
2017-08-05 00:23:20 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,11 @@
+
+{
+    "moduleId" : "grabowCommuter/vinfo",
+    "type" : "Website",
+    "name" : "VInfo",
+    "uri" : "http://www.verkehrsinfo.de/httpsmobil/index.php?c=staulist&lat=#lat#&lon=#lng#",
+    "stratTrigger" : "Verkehrsinfo",
+    "endTrigger" : "",
+    "startTag" : "li",
+    "endTag" : "li"
+}
