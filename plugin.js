9e14bc26fd5e98e8923cad34462daff0cff9d87a
maps4all <maps4all007@yandex.com>
2017-09-02 15:36:30 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 
 {
-    "moduleId" : "grabowCommuter/vinfo",
+    "moduleId" : "maps4all/germany_verkehrsinfo_ortsabhaengig",
     "type" : "Website",
     "name" : "VInfo",
     "uri" : "http://www.verkehrsinfo.de/httpsmobil/index.php?c=staulist&lat=#lat#&lon=#lng#",
